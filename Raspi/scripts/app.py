a3b3f0da21690b9c7580f5aa297b0b22b8fd997e
Aristeo Montoya <aristeomontoya98@gmail.com>
2021-01-13 13:34:59 -0700

    Incluido directorio de scripts
    Implementación de logging para script de oferteros

---
+++ Raspi/scripts/app.py
@@ -0,0 +1,50 @@
+import time
+import logging
+import sys
+import re
+import os
+import pyinotify
+from pdf2image import convert_from_path as pdf_converter
+
+
+def convert_pdf(event):
+    event_type = event.maskname
+    logging.info(f'Evento: {event_type}')
+
+    if event_type in ('IN_MOVED_TO', 'IN_CREATE'):
+        logging.info('Entre los eventos válidos')
+        file = event.name
+        logging.info(f'Nombre del archivo: {file}')
+
+        # Comprobando que el archivo termine en pdf
+        if re.search('.*\.pdf$', file):
+            logging.info('Es PDF')
+            file_path = watch_dir + '/' + file
+            dir_name = file_path[:-4]
+            try:
+                images = pdf_converter(file_path)
+                os.mkdir(dir_name)
+                for i, img in enumerate(images):
+                    img.save(f'{dir_name}/Ofertero ({i + 1}).jpg', 'JPEG')
+                os.rename(file_path, dir_name + '/' + file)
+
+            except Exception as e:
+                logging.error('Error')
+                time.sleep(2)
+                convert_pdf(event)
+
+
+if __name__ == '__main__':
+    # Archivo de log
+    logging.basicConfig(filename='log_oferteros.log', level=logging.INFO)
+
+    # Directorio a escuchar obtenido de argumentos
+    watch_dir = sys.argv[1]
+    while True:
+        watcher = pyinotify.WatchManager()
+
+        notifier = pyinotify.Notifier(watcher, default_proc_fun=convert_pdf)
+        watcher.add_watch(watch_dir, pyinotify.ALL_EVENTS)
+
+        # Comienza a escuchar eventos
+        notifier.loop()
