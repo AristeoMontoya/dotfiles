c2bcdd67d637a74757d8fe07af8dc3343e0e78cb
Aristeo Montoya <aristeomontoya98@gmail.com>
2021-01-17 16:47:27 -0700

    Log de oferteros separado
    La ruta del log se lee de las variables de ambiente

--- Raspi/scripts/app_oferteros.py
+++ Raspi/scripts/app_oferteros.py
@@ -1,11 +1,20 @@
+'''Conversión de PDF a imágenes'''
 from datetime import datetime
+import os
 import time
 import logging
 import sys
 import re
-import os
 import pyinotify
 from pdf2image import convert_from_path as pdf_converter
+
+
+def get_logdir():
+    log_file = os.environ.get('LOGPDF')
+    if not log_file:
+        log_file = 'log_pdf.log'
+
+    return log_file
 
 
 def split_path(path):
@@ -14,6 +23,8 @@ def split_path(path):
 
 
 def dir_listener(watch_dir):
+    timestamp = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
+    logging.info('---- %s ----', timestamp)
     logging.info('Escuchando en: %s', watch_dir)
     while True:
         watcher = pyinotify.WatchManager()
@@ -39,7 +50,9 @@ def event_handler(event):
 def convert_pdf(directory, file):
     # Comprobando que el archivo termine en pdf
     if re.search('.*\.pdf$', file):
-        logging.info('Es PDF: %s', file)
+        timestamp = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
+        logging.info('---- %s ----', timestamp)
+        logging.info('Archivo: %s', file)
         file_name = file[:-4]
         file_path = directory + '/' + file
         dir_name = file_path[:-4]
@@ -51,7 +64,7 @@ def convert_pdf(directory, file):
             os.rename(file_path, dir_name + '/' + file)
 
         except Exception as e:
-            timestamp = datetime.now().strftime("%Y/%m/%d - %H:%M%:%S")
+            timestamp = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
             logging.error('---- %s ----', timestamp)
             logging.error('Error: %s', e)
             time.sleep(2)
@@ -60,15 +73,15 @@ def convert_pdf(directory, file):
 
 if __name__ == '__main__':
     # Archivo de log
-    log_file = '/home/pi/scripts/log_oferteros.log'
+    log_file = get_logdir()
     logging.basicConfig(filename=log_file, level=logging.INFO)
 
     # Directorio a escuchar obtenido de argumentos
-    path = sys.argv[1]
+    received_path = sys.argv[1]
 
-    if os.path.isdir(path):
-        watch_dir = path
-        dir_listener(path)
+    if os.path.isdir(received_path):
+        watch_dir = received_path
+        dir_listener(received_path)
     else:
-        file_path = split_path(path)
+        file_path = split_path(received_path)
         convert_pdf(*file_path)
