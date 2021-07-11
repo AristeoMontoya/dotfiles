2217ae3307112215845cc2cf5a0170cf3d8c0e39
Aristeo Montoya <aristeomontoya98@gmail.com>
2021-07-10 23:47:48 -0600

    TSServer

--- nvim/lua/lsp/js.lua
+++ nvim/lua/lsp/js.lua
@@ -7,7 +7,10 @@ require'lspconfig'.tsserver.setup {
 	settings = {documentFormatting = false},
 	handlers = {
 		["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-			update_in_insert = true
+			update_in_insert = true,
+			virtual_text = { spacing = 0, prefix = "" },
+			signs = true,
+			underline = true,
 		})
 	}
 }
