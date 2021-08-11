d796ac9b6f4bc7ef6a649e83daa220426bf1d934
Aristeo Montoya <aristeomontoya98@gmail.com>
2021-08-11 03:35:45 -0600

    indent
    Corrección de contexto

--- nvim/lua/nv_blankline/init.lua
+++ nvim/lua/nv_blankline/init.lua
@@ -1,12 +1,14 @@
 require("indent_blankline").setup {
 	char = "▏",
-	buftype_exclude = { "terminal" },
+	buftype_exclude = { "terminal", "help" },
 	use_treesitter = true,
 	show_trailing_blankline_indent = false,
-	-- De momento no funciona el resaltado de contexto
-	-- show_current_context = true,
-	-- context_patterns = {
-	-- 	"declaration", "expression", "pattern", "primary_expression",
-	-- 	"statement", "switch_body", "function"
-	-- },
+	show_current_context = true,
+ 	context_patterns = {
+		"declaration", "expression", "pattern", "primary_expression",
+		"statement", "switch_body", "function"
+	}
 }
+
+-- Autocomand porque es cargado bajo demanda
+vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
