2e567047350bc0d29c70167b231e0e71d2e9c772
Aristeo Montoya <aristeomontoya98@gmail.com>
2021-08-10 23:37:51 -0600

    indent
    Se eliminó el resaltado de contexto por ahora

--- nvim/lua/nv_blankline/init.lua
+++ nvim/lua/nv_blankline/init.lua
@@ -2,9 +2,11 @@ require("indent_blankline").setup {
 	char = "▏",
 	buftype_exclude = { "terminal" },
 	use_treesitter = true,
-	show_current_context = true,
-	context_patterns = {
-		"declaration", "expression", "pattern", "primary_expression",
-		"statement", "switch_body", "function"
-	}
+	show_trailing_blankline_indent = false,
+	-- De momento no funciona el resaltado de contexto
+	-- show_current_context = true,
+	-- context_patterns = {
+	-- 	"declaration", "expression", "pattern", "primary_expression",
+	-- 	"statement", "switch_body", "function"
+	-- },
 }
