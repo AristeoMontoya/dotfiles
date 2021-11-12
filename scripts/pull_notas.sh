bd7654b1efdd9e107fee2b140527c4dd8e86deb7
Aristeo Montoya <aristeomontoya98@gmail.com>
2021-11-12 00:02:26 -0700

    Script de sincronización de notas

---
+++ scripts/pull_notas.sh
@@ -0,0 +1,14 @@
+notes="~/notas"
+cd $notes
+message="AUTO $(date +'%Y-%m-%d %H:%M')"
+
+git add .
+git commit -m "$message"
+git pull
+if [ $? -eq 0 ]; then
+	echo "Sin conflicto"
+	git push
+	exit 0
+else
+	exit 1
+fi
