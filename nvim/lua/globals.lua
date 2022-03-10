22372b3dfe6315a0b68561581cc98a211c0ac09e
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-03-09 20:48:56 -0700

    Add node installed validation

--- nvim/lua/globals.lua
+++ nvim/lua/globals.lua
@@ -3,6 +3,9 @@ DATA_PATH = V.fn.stdpath('data')
 CACHE_PATH = V.fn.stdpath('cache')
 CONFIG_PATH = V.fn.stdpath('config')
 VSCODE = V.api.nvim_eval('exists("g:vscode")')
--- Cambiar para alternar la configuración entre
+-- Pick between lsp or CoC
 -- CoC o LSP
 COC = true
+
+-- Needed in order to enable CoC or LSP
+HAS_NODE = true
