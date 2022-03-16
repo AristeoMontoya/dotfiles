ae4798a741556d1a34aa353ad65d117926675a89
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-03-15 18:45:26 -0700

    Update snippets

--- nvim/lua/nv_ultisnips/init.lua
+++ nvim/lua/nv_ultisnips/init.lua
@@ -1,3 +1,3 @@
 -- No son necesarios estos dos, pero los tuve que poner para que Compe no explote.
-V.g['UltiSnipsJumpForwardTrigger'] = '<c-l>'
-V.g['UltiSnipsJumpBackwardTrigger'] = '<c-h>'
+V.g['UltiSnipsJumpForwardTrigger'] = '<A-l>'
+V.g['UltiSnipsJumpBackwardTrigger'] = '<A-h>'
