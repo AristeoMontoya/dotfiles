5e42eeba4a311e41997e39e1c85fe82ad4dd68e7
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-03-15 21:40:01 -0700

    Fix setup function for language server being called twice

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,22 +8,11 @@ if not IS_VSCODE then
 	if not COC then
 		V.cmd('source ~/.config/nvim/vimscript/lsp_keymaps.vim')
 		-- LSP
-		-- require('lsp')
-		-- require('lsp.python')
-		-- require('lsp.bash')
-		-- require('lsp.java')
-		-- require('lsp.js')
-		-- require('lsp.lua-ls')
-		-- require('lsp.go')
-		-- require('lsp.efm')
-		-- require('lsp.html')
-		-- require('lsp.emmet')
-		-- require('lsp.vue')
+		require('lsp')
 	end
 	V.cmd('source ~/.config/nvim/vimscript/keymaps.vim')
 	V.cmd('source ~/.config/nvim/vimscript/autocommands.vim')
 	require('nv_base16')
-
 else
 	V.cmd('source ~/.config/nvim/vimscript/vscodemaps.vim')
 end
