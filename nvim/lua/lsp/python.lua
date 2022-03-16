d308d27379c192bad841c728dd3dea88e0061b1c
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-03-15 21:40:01 -0700

    Fix setup function for language server being called twice

--- nvim/lua/lsp/python.lua
+++ nvim/lua/lsp/python.lua
@@ -1,7 +1,7 @@
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(V.lsp.protocol.make_client_capabilities())
 
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['pyright'].setup {
-	capabilities = capabilities
+require('lspconfig').pyright.setup {
+-- 	capabilities = capabilities
 }
