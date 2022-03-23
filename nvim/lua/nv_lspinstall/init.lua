f2639553d58fafaf3041b7565ea8fd0530cbe02c
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-03-22 22:49:14 -0700

    Add case for jdlst in order to ignore it

--- nvim/lua/nv_lspinstall/init.lua
+++ nvim/lua/nv_lspinstall/init.lua
@@ -1,5 +1,8 @@
 local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
     local opts = {}
+	if server.name == "jdtls" then
+		return
+	end
     server:setup(opts)
 end)
