ae4ea0e40c12f8d5563efae65b6e931f6e9f26a4
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-03-25 20:34:17 -0700

    Document highlight and cmp fix

--- nvim/lua/nv_cmp/init.lua
+++ nvim/lua/nv_cmp/init.lua
@@ -61,7 +61,7 @@ cmp.setup({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<TAB>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	},
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
