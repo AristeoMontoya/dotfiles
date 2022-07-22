2cbe48efda674ab1834a711ac1caafac17e850a9
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-07-21 18:26:01 -0600

    Update java snippets

--- nvim/.config/nvim/snippets/java.lua
+++ nvim/.config/nvim/snippets/java.lua
@@ -10,12 +10,13 @@ local i = ls.insert_node
 local t = ls.text_node
 local d = ls.dynamic_node
 local c = ls.choice_node
+local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 local ts_locals = require "nvim-treesitter.locals"
 local ts_utils = require "nvim-treesitter.ts_utils"
 
-local get_node_text = vim.treesitter.get_node_text
+local get_node_text = V.treesitter.get_node_text
 
 local function get_filename()
 	return V.fn.expand('%:t:r')
@@ -31,12 +32,16 @@ ls.add_snippets(
 		{}
 	}}
 	]], { i(0) })),
-	
+
 	s("cbplate", fmt(
 	[[
 	public class {} {{
 		{}
 	}}
-	]], { t(get_filename()), i(0) }
+	]], { d(1, function(_, snip)
+		return sn(nil, {
+			i(1, V.fn.substitute(snip.env.TM_FILENAME, "\\..*$", "", "g")),
+		})
+	end, { 1 }), i(0) }
 	))
 })
