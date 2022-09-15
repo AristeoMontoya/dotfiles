dbcb0387615ddba64d5f0685a0816eab3aea845c
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-09-14 21:37:08 -0600

    Ignore compiled classes for telescope
    Lua uses some weird regex thing

--- nvim/.config/nvim/lua/nv_telescope/init.lua
+++ nvim/.config/nvim/lua/nv_telescope/init.lua
@@ -31,7 +31,7 @@ require('telescope').setup{
 		},
 		file_sorter =  require'telescope.sorters'.get_fuzzy_file,
 		file_ignore_patterns = {
-			"*.class"
+			"%.class"
 		},
 		generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
 		winblend = 0,
