2511cafcdb61c11ead3c3d7ffa8dcb1d4e145d8d
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-12-13 21:56:56 -0700

    Add fstring snippet for python

--- nvim/.config/nvim/snippets/python.lua
+++ nvim/.config/nvim/snippets/python.lua
@@ -2,7 +2,6 @@ local ls = require "luasnip"
 
 require("luasnip.session.snippet_collection").clear_snippets "go"
 
--- local snippet = ls.s
 local snippet_from_nodes = ls.sn
 
 local s = ls.s
@@ -10,12 +9,21 @@ local i = ls.insert_node
 local t = ls.text_node
 local d = ls.dynamic_node
 local c = ls.choice_node
+local f = ls.function_node
+local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 local ts_locals = require "nvim-treesitter.locals"
 local ts_utils = require "nvim-treesitter.ts_utils"
 
 local get_node_text = V.treesitter.get_node_text
+
+local filename = function()
+	return f(function(_args, snip)
+		local name = vim.split(snip.snippet.env.TM_FILENAME, ".", true)
+		return name[1] or ""
+	end)
+end
 
 ls.add_snippets(
 "python", {
@@ -24,4 +32,12 @@ ls.add_snippets(
 	if __name__ == '__main__':
 		{}
 	]], { i(0) })),
+
+	s("fstring", fmt(
+	[[
+	f'{}'
+	]], {
+		i(0)
+	}
+	))
 })
