5c91d19b94d68b94d1d1c61a12a3b207ef185e04
Aristeo Montoya <aristeomontoya98@gmail.com>
2022-12-23 21:31:36 -0700

    Disable tmux integration for neovim on zoom

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -43,12 +43,9 @@ return require('packer').startup(function(use)
 		use {
 			'numToStr/Navigator.nvim',
 			config = function()
-				require('Navigator').setup()
-				vim.keymap.set({'n', 't'}, '<C-h>', '<CMD>NavigatorLeft<CR>')
-				vim.keymap.set({'n', 't'}, '<C-l>', '<CMD>NavigatorRight<CR>')
-				vim.keymap.set({'n', 't'}, '<C-k>', '<CMD>NavigatorUp<CR>')
-				vim.keymap.set({'n', 't'}, '<C-j>', '<CMD>NavigatorDown<CR>')
-				vim.keymap.set({'n', 't'}, '<C-p>', '<CMD>NavigatorPrevious<CR>')
+				require('Navigator').setup({
+					disable_on_zoom = true
+				})
 			end
 		}
 		-- Packer can manage itself
