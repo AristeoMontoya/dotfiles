0c5dbd3e5319b3a14b2b9dc43b33a9e803ac2ea6
Aristeo Montoya <aristeomontoya98@gmail.com>
2023-07-13 22:50:27 -0700

    Update nerd font glyphs. Removed deprecated glyphs

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -2,15 +2,15 @@ require('globals')
 -- TODO figure out why this don't work
 V.fn.sign_define(
 	"DiagnosticSignError",
-	{texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError"}
+	{texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError"}
 )
 V.fn.sign_define(
 	"DiagnosticSignWarn",
-	{texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarning"}
+	{texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarning"}
 )
 V.fn.sign_define(
 	"DiagnosticSignHint",
-	{texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint"}
+	{texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint"}
 )
 V.fn.sign_define(
 	"DiagnosticSignInformation",
