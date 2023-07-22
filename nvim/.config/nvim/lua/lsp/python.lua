71fa7d597464fec286f676874f9ac632b86c5c73
Aristeo Montoya <aristeomontoya98@gmail.com>
2023-07-22 09:18:43 -0700

    Remove old lsp configs and add angularls

--- nvim/.config/nvim/lua/lsp/python.lua
+++
@@ -1,7 +0,0 @@
--- Setup lspconfig.
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(V.lsp.protocol.make_client_capabilities())
-
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig').pyright.setup {
--- 	capabilities = capabilities
-}
