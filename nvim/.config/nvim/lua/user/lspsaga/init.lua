1b786252d5d3396820a27e8ad7b2ada55a378299
Aristeo Montoya Gonzalez <aristeo.gonzalez@verato.com>
2023-08-11 16:37:13 -0700

    Remove bulb from lsp saga

--- nvim/.config/nvim/lua/user/lspsaga/init.lua
+++ nvim/.config/nvim/lua/user/lspsaga/init.lua
@@ -1,8 +1,14 @@
 require('lspsaga').setup({
+	lightbulb = {
+		enable = false
+	},
 	symbol_in_winbar = {
 		enable = true,
 		separatos = ' > ',
 		show_file = false,
 		color_mode = true
+	},
+	rename = {
+		in_select = false,
 	}
 })
