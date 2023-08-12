efbd2ca399871b455a4c4be991bce9f6f477f53f
Aristeo Montoya <aristeomontoya98@gmail.com>
2023-08-12 00:11:03 -0700

    Add overrides to mason in order to ignore lsp servers

---
+++ nvim/.config/nvim/lua/lsp/default_servers.lua
@@ -0,0 +1,16 @@
+return {
+	"bashls",
+	"cssls",
+	"dockerls",
+	"emmet_ls",
+	"gradle_ls",
+	"groovyls",
+	"html",
+	"jsonls",
+	"tsserver",
+	"lua_ls",
+	"pyright",
+	"vimls",
+	"lemminx",
+}
+
--- nvim/.config/nvim/lua/lsp/mason.lua
+++ nvim/.config/nvim/lua/lsp/mason.lua
@@ -1,3 +1,6 @@
+local overrides_ok, overrides = pcall(require, "lsp.overrides")
+local filter_ok, filter = pcall(require, "utils.filter_tables")
+
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
 	return
@@ -10,20 +13,16 @@ end
 
 mason.setup()
 
+local defaults = require("lsp.default_servers")
+
+local ensure_installed = {}
+
+if overrides_ok and filter_ok then
+	ensure_installed = filter(defaults, overrides.ignored)
+else
+	ensure_installed = defaults
+end
+
 mason_lspconfig.setup({
-	ensure_installed = {
-		"bashls",
-		"cssls",
-		"dockerls",
-		"emmet_ls",
-		"gradle_ls",
-		"groovyls",
-		"html",
-		"jsonls",
-		"tsserver",
-		"lua_ls",
-		"pyright",
-		"vimls",
-		"lemminx"
-	}
+	ensure_installed = ensure_installed
 })
