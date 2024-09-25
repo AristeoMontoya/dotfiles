0ee4853e824baf0d3f3d4805bf4adb5b1fc2a8fe
Aristeo Montoya <aristeomontoya98@gmail.com>
2024-09-24 22:27:00 -0700

    Add lsp doc border

--- nvim/.config/nvim/lua/plugins/ui/noice.lua
+++ nvim/.config/nvim/lua/plugins/ui/noice.lua
@@ -61,6 +61,9 @@ return {
 				},
 			},
 		},
+		presets = {
+			lsp_doc_border = true
+		}
 	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
