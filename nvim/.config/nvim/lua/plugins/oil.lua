cb4ca7c5cfb45bc6ed855382eacf88c3108b5856
Aristeo Montoya <aristeomontoya98@gmail.com>
2024-09-26 13:51:53 -0700

    Add oil keymaps

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,32 +1,47 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {
-		columns = {
-			"icon",
-		},
-		delete_to_trash = true,
-		keymaps = {
-			["g?"] = "actions.show_help",
-			["<CR>"] = "actions.select",
-			["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
-			["<C-h>"] = {
-				"actions.select",
-				opts = { horizontal = true },
-				desc = "Open the entry in a horizontal split",
+	opts = function()
+		local keymap = vim.keymap
+
+		keymap.set(
+			"n",
+			"<leader>fo",
+			"<cmd>lua require('oil').toggle_float()<cr>",
+			{ silent = true, noremap = true, desc = "Open oil in a floating pane" }
+		)
+
+		return {
+			columns = {
+				"icon",
 			},
-			["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-			["<C-p>"] = "actions.preview",
-			["<C-c>"] = "actions.close",
-			["<C-l>"] = "actions.refresh",
-			["<BS>"] = "actions.parent",
-			["_"] = "actions.open_cwd",
-			["`"] = "actions.cd",
-			["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
-			["gs"] = "actions.change_sort",
-			["gx"] = "actions.open_external",
-			["g."] = "actions.toggle_hidden",
-			["g\\"] = "actions.toggle_trash",
-		},
-		use_default_keymaps = false,
-	},
+			delete_to_trash = true,
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = {
+					"actions.select",
+					opts = { vertical = true },
+					desc = "Open the entry in a vertical split",
+				},
+				["<C-h>"] = {
+					"actions.select",
+					opts = { horizontal = true },
+					desc = "Open the entry in a horizontal split",
+				},
+				["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+				["<C-p>"] = "actions.preview",
+				["<C-c>"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["<BS>"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+				["gs"] = "actions.change_sort",
+				["gx"] = "actions.open_external",
+				["g."] = "actions.toggle_hidden",
+				["g\\"] = "actions.toggle_trash",
+			},
+			use_default_keymaps = false,
+		}
+	end,
 }
