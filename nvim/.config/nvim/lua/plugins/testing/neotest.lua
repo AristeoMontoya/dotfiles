a530ee585bf3758431d3ba47bc31ea67c8d5d1c7
Aristeo Montoya <aristeomontoya98@gmail.com>
2024-11-29 16:40:41 -0700

    nvim - Remove comments

--- nvim/.config/nvim/lua/plugins/testing/neotest.lua
+++ nvim/.config/nvim/lua/plugins/testing/neotest.lua
@@ -23,16 +23,9 @@ return {
 
 		return {
 			adapters = {
-				-- require("neotest-python")({
-				-- 	dap = { justMyCode = false },
-				-- }),
 				neotest_golang({
 					dap = { justMyCode = false },
 				}),
-				-- require("neotest-plenary"),
-				-- require("neotest-vim-test")({
-				-- 	ignore_file_types = { "python", "vim", "lua" },
-				-- }),
 			},
 		}
 	end,
