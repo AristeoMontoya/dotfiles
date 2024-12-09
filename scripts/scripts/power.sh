349d84cde2fba5aeb584d81378ae827c41c45fdb
Aristeo Montoya <aristeomontoya98@gmail.com>
2024-12-09 15:59:42 -0700

    scripts - Update battery script to tune conservative governor

--- scripts/scripts/power.sh
+++ scripts/scripts/power.sh
@@ -7,7 +7,6 @@ if [[ "$1" == "BAT" || "$1" == "AC" ]]; then
 	STATE="$1"
 fi
 
-
 if [[ $STATE == "" ]]; then
 	if [[ $(grep -i discharging /sys/class/power_supply/"$BAT"/status) == "" ]]; then
 		STATE="AC"
@@ -21,6 +20,9 @@ echo $STATE
 if [[ $STATE == "BAT" ]]; then
 	echo "Discharging. Set governor to conservative."
 	cpupower frequency-set -g conservative
+	# Changing governor sensitivity. By default 20 and the lower the value,
+	# the quicker it increases frequency based on load.
+	echo -n 15 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold
 elif [[ $STATE == "AC" ]]; then
 	echo "Charging. Set governor to schedutil."
 	cpupower frequency-set -g schedutil 
