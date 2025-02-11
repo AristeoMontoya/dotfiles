bdd9942d81c1d9c59417d63ec553afd75c0571c4
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-02-10 21:30:13 -0700

    nvim - Add breakpoint rejected icon

--- nvim/.config/nvim/lua/plugins/dap/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap/dap.lua
@@ -42,6 +42,8 @@ return {
 		})
 
 		vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DiagnosticSignInfo", linehl = "", numhl = "" })
+
 
 		vim.fn.sign_define(
 			"DapBreakpointCondition",
