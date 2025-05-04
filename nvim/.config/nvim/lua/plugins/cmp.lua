a6772a7679c5f5adc86eb3c8359350680d22491f
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-05-04 00:35:10 -0700

    nvim - fix cmp mappings

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -87,11 +87,11 @@ return {
 				end,
 			},
 			mapping = {
-				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 				["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-X><C-O>"] = cmp.mapping.complete(),
 				["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 				["<C-e>"] = cmp.mapping({
