436795bf7176586458baa1c56f2dfe233e86c3aa
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-07-18 15:01:16 -0700

    nvim - Remove osc52 detection

--- nvim/.config/nvim/lua/settings/clipboard.lua
+++ nvim/.config/nvim/lua/settings/clipboard.lua
@@ -1,21 +1,2 @@
-local is_tmux_session = vim.env.TERM_PROGRAM == "tmux" -- Tmux is its own clipboard provider which directly works.
-
-if vim.env.SSH_TTY and not is_tmux_session then
-	local function paste()
-		return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-	end
-	local osc52 = require("vim.ui.clipboard.osc52")
-	vim.g.clipboard = {
-		name = "OSC 52",
-		copy = {
-			["+"] = osc52.copy("+"),
-			["*"] = osc52.copy("*"),
-		},
-		paste = {
-			["+"] = paste,
-			["*"] = paste,
-		},
-	}
-end
-
+-- osc52 detection is no longer needed, it is builtin now
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
