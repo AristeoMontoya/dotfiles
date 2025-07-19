b3452811fd4021add0a05c3d6b1c39b2003f2ce1
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-07-18 17:37:25 -0700

    nvim - Add mechanism to allow local lsp server overrides
    I don't want to expose repo structures or build tags used at work in this repo

---
+++ nvim/.config/nvim/lua/user/overrides/lsp/template.configs.lua
@@ -0,0 +1,14 @@
+-- Intended to be coppied without "template" prefix.
+-- The file wont be traccked by git.
+-- Values added here will be merged with those in lsp/configs.lua
+-- and take presedence over them.
+-- The example python config here is redundant.
+return {
+	pyright = {
+		settings = {
+			pyright = {
+				autoImportCompletion = true,
+			},
+		},
+	},
+}
