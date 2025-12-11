24b9a12436b82832c6f95ce6324729e346a0661d
Aristeo Montoya <aristeomontoya98@gmail.com>
2025-12-11 16:54:19 -0700

    neovim - Add feature list with overrides

---
+++ nvim/.config/nvim/lua/defaults/features/init.lua
@@ -0,0 +1,9 @@
+--- @class model.Features
+--- @field ai boolean?
+--- @field java boolean?
+local features = {
+	ai = true,
+	java = true
+}
+
+return features
---
+++ nvim/.config/nvim/lua/defaults/model/init.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+--- @class model.Features
+--- @type table<string, boolean>
+local features = {
+	ai = true,
+	copilot = true
+}
+
+M.Features = features
+
+return M
